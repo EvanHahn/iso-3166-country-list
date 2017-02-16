9b5f6b23719e759181d2d394865e3649870cf7b7
Evan Hahn <me@evanhahn.com>
2017-02-16 12:05:53 -0800

    Convert lists tests from CoffeeScript

--- test/list.js
+++ test/list.js
@@ -1,19 +1,51 @@
 var assert = require('chai').assert
 var countryList = require('..')
 
-describe('lookups', function () {
-  it('should be able to look up properly-capitalized Germany', function () {
-    assert.equal(countryList.name('DE'), 'Germany')
-    assert.equal(countryList.code('Germany'), 'DE')
-  })
+var COUNTRY_COUNT = 249
 
-  it('should be able to look up improperly-capitalized Germany', function () {
-    assert.equal(countryList.name('dE'), 'Germany')
-    assert.equal(countryList.code('germany'), 'DE')
-  })
+describe('lists', function () {
+  describe('master list', function () {
+    it('contains ' + COUNTRY_COUNT + ' countries', function () {
+      assert.lengthOf(countryList, COUNTRY_COUNT)
+    })
 
-  it('should return undefined with unknown values', function () {
-    assert.isUndefined(countryList.name('something-unknown'))
-    assert.isUndefined(countryList.code('something-unknown'))
+    it('contains a bunch of countries', function () {
+      assert(countryList.find(function (country) {
+        return country.code === 'ZW' && country.name === 'Zimbabwe'
+      }))
+      assert(countryList.find(function (country) {
+        return country.code === 'AF' && country.name === 'Afghanistan'
+      }))
+      assert(countryList.find(function (country) {
+        return country.code === 'DE' && country.name === 'Germany'
+      }))
+      assert(countryList.find(function (country) {
+        return country.code === 'ME' && country.name === 'Montenegro'
+      }))
+    })
+
+    describe('codes list', function () {
+      it('contains ' + COUNTRY_COUNT + ' codes', function () {
+        assert.lengthOf(countryList.codes, COUNTRY_COUNT)
+      })
+
+      it('contains various countries', function () {
+        assert.include(countryList.codes, 'ES')
+        assert.include(countryList.codes, 'US')
+        assert.include(countryList.codes, 'BS')
+      })
+    })
+
+    describe('names list', function () {
+      it('contains ' + COUNTRY_COUNT + ' names', function () {
+        assert.lengthOf(countryList.names, COUNTRY_COUNT)
+      })
+
+      it('contains various countries', function () {
+        assert.include(countryList.names, 'Bahamas')
+        assert.include(countryList.names, 'Germany')
+        assert.include(countryList.names, 'Spain')
+      })
+    })
   })
 })
