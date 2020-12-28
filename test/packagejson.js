b785a54f1ed43ffe761a8cd7f14fd5ff798f75d4
Evan Hahn <me@evanhahn.com>
2020-12-28 15:22:41 -0600

    Remove assertion that there are no dependencies

--- test/packagejson.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert
-const pkg = require('../package.json')
-
-describe('package.json', function () {
-  it('has no dependencies', function () {
-    assert.isUndefined(pkg.dependencies)
-  })
-})
